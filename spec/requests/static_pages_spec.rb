df7733bedcf9987fab43f6974b64f3fb80597b7d
zak956 <zak956@gmail.com>
2014-04-04 16:46:23 +0300

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Home')
+    end
+  end
+  
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Help')
+    end
+  end
+  
+  describe "About page" do
+    it "should have content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About')
+    end
+  end
+  
+end
